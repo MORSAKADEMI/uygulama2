5161a38d9cde20a220b37ab05fa1dbf2fee96378
Burak Üstün <79836033+burakbfm@users.noreply.github.com>
2022-08-19 18:31:58 +0300

    Create 2-Uygulama.py

---
+++ 2-Uygulama.py
@@ -0,0 +1,21 @@
+website = "https://morsakademi.com"
+course = "Python Kursu: Robotik Kodlama eğitmenliği dersleri (45 Saat)"
+
+# 1 - "Course" karakter dizisinde kaç karakter bulunmaktadır ?
+
+# 2 - "website" içinden www karakterlerini alın.
+
+# 3 - "website" içinden com karakterlerini alın.
+
+# 4 - "course" içinden ilk 15 ve son 15 karakterlerini alıN
+
+# 5 - "course" ifadesindeki karakterleri tersten yazdırın.
+
+name, surname, age, job= "Burak", "Üstün", 25, "Eğitimci"
+
+# 6 - Yukarıda verilen değişkenler ile ekrana aşağıdaki ifadeyi yazdırın.
+#     "Benim adım Burak Üstün, Yaşım 25 ve mesleğim Eğitimci" (3 farklı yöntemler)
+
+# 7 - "Hello World" ifadesindeki w harfini "W" ile değiştirin.
+
+# 8 - "abc" ifadesini yan yana 3 defa yazdırın.
